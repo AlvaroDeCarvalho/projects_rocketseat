e793ce089475b5711c62fd8376603636e7b6c601
Alvaro Carvalho <alvaro@novetech.com.br>
2024-08-07 23:22:11 -0300

    finalização da pagina Home e inicialização do historico

--- modulo_2/02-ignite-timer/src/pages/Home/index.tsx
+++ modulo_2/02-ignite-timer/src/pages/Home/index.tsx
@@ -8,10 +8,31 @@ export function Home() {
       <form action="">
         <S.FormContainer>
           <label htmlFor="tasks">Vou Trabalhar em</label>
-          <input type="text" id="tasks" />
+
+          <datalist id="tasks-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+            <option value="banana 5" />
+          </datalist>
+
+          <S.TasksInput
+            type="text"
+            id="tasks"
+            list="tasks-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="text" id="minutesAmount" />
+          <S.TasksAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="- 00 +"
+            step={5}
+            min={5}
+            max={60}
+          />
           <span>minutos.</span>
           <S.CountdownContainer>
             <span>0</span>
@@ -21,7 +42,7 @@ export function Home() {
             <span>0</span>
           </S.CountdownContainer>
         </S.FormContainer>
-        <S.PlayButoon type="submit">
+        <S.PlayButoon type="submit" disabled={false}>
           <Play size={24} /> Enviar
         </S.PlayButoon>
       </form>
--- modulo_2/02-ignite-timer/src/pages/Home/styles.ts
+++ modulo_2/02-ignite-timer/src/pages/Home/styles.ts
@@ -2,14 +2,14 @@ import styled from 'styled-components'
 
 export const HomeContainer = styled.main`
   flex: 1;
-
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
 
   form {
-    max-width: 646px;
+    max-width: 800px;
+
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -28,6 +28,7 @@ export const FormContainer = styled.div`
 `
 
 export const CountdownContainer = styled.div`
+  margin-top: 3rem;
   font-family: 'Roboto Mono', monospace;
   font-size: 10rem;
   line-height: 8rem;
@@ -51,17 +52,57 @@ export const Separator = styled.div`
   justify-content: center;
   align-items: center;
   color: ${({ theme }) => theme['green-500']};
-
   overflow: hidden;
 `
 
+const BaseInput = styled.input`
+  background-color: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme['green-500']};
+  font-size: 1.125rem;
+  font-weight: 1.25rem;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme['green-500']};
+  }
+`
+
+export const TasksInput = styled(BaseInput)`
+  flex: 1;
+  max-width: auto;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
+`
+export const TasksAmountInput = styled(BaseInput)`
+  width: 4rem;
+`
+
 export const PlayButoon = styled.button`
-  width: 100%;
-  overflow: hidden;
+  width: 35.5rem;
   background-color: ${(props) => props.theme['green-500']};
+
+  cursor: pointer;
+  overflow: hidden;
   color: #fff;
   font-size: 1.25rem;
+
   padding: 1rem 2rem;
+
   border-radius: 8px;
   border: none;
+
+  &:disabled {
+    background-color: ${(props) => props.theme['green-500']};
+    cursor: not-allowed;
+    opacity: 0.7;
+  }
 `
