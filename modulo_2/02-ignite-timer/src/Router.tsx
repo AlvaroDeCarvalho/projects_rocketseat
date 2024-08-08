85256d02b7b2f1cdd25c48f3d99fb7738395ed1d
Alvaro Carvalho <alvaro@novetech.com.br>
2024-08-07 23:23:37 -0300

    implementando o history no sistemas de rotas

--- modulo_2/02-ignite-timer/src/Router.tsx
+++ modulo_2/02-ignite-timer/src/Router.tsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
+
 import { DefaultLayout } from './layout/DefaultLayout'
 import { Home } from './pages/Home/index'
+import { History } from './pages/History'
 
 export function RouterContainer() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/history" element={<div>History</div>} />
+        <Route path="/history" element={<History />} />
       </Route>
     </Routes>
   )
