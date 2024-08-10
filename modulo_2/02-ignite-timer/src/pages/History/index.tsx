d79dbec1d5712ab99c68292bd5aa3042d5885f24
Alvaro Carvalho <alvaro@novetech.com.br>
2024-08-10 15:22:24 -0300

    finalização do History

--- modulo_2/02-ignite-timer/src/pages/History/index.tsx
+++ modulo_2/02-ignite-timer/src/pages/History/index.tsx
@@ -1,3 +1,49 @@
+import * as S from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <S.HistoryContainer>
+      <h1>Meu historico</h1>
+
+      <S.HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Conserto de debitos tecnicos</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Conserto de debitos tecnicos</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Conserto de debitos tecnicos</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Conserto de debitos tecnicos</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+          </tbody>
+        </table>
+      </S.HistoryList>
+    </S.HistoryContainer>
+  )
 }
